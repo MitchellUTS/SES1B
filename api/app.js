98d75963b76c1f3f4a89bfbff8600304d52facbc
ryandawson <dawson.ryan24@gmail.com>
2019-09-13 12:44:45 +1000

    help

--- api/app.js
+++ api/app.js
@@ -7,12 +7,16 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require('./routes/testAPI');
+var ejs = require('ejs');
+var paypal = require('paypal-rest-sdk');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
 
 app.use(logger('dev'));
 app.use(cors());
@@ -42,3 +46,82 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'AYrqSddTaoMoT0YNT_g45A1D03qeu37-NEUS2CyIx6ZTqxi4nFnsScO7sagv63vr1xVLpjEL-EiV0_OX',
+  'client_secret': 'EBLPCMU4LF5SzH5rUEZkYT3tZBSyL5b3eUFwvjClSJTA8HzKKJTSKguRBlKkNphMbDjIueMeLbepNeSa'
+  });
+
+app.post('/pay', (req, res) => {
+  console.log("TEST");    
+  const create_payment_json = {
+    "intent": "sale",
+    "payer": {
+      "payment_method": "paypal"
+    },
+    "redirect_urls": {
+      "return_url": "http://localhost:3000/success",
+      "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+      "item_list": {
+        "items": [{
+          "name": "Intel Core i5",
+          "sku": "001",
+          "price": "299.00",
+          "currency": "AUD",
+          "quantity": 1
+        }]
+      },
+      "amount": {
+        "currency": "AUD",
+        "total": "299.00"
+      },
+      "description": "A CPU"
+    }]
+  };
+    
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+      throw error;
+    } else {
+      for(let i = 0;i < payment.links.length;i++){
+        if(payment.links[i].rel === 'approval_url'){
+          res.redirect(payment.links[i].href);
+        }
+      }
+    }
+  });
+  
+});
+
+app.get('/success', (req, res) => {
+  const payerId = req.query.PayerID;
+  const paymentId = req.query.paymentId;
+  const payment = req.query.payment;
+    
+  const execute_payment_json = {
+    "payer_id": payerId,
+    "transactions": [{
+      "amount": {
+        "currency": "AUD",
+        "total": "299.00"
+      }
+    }]
+  };
+    
+  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+    if (error) {
+        console.log(error.response);
+      throw error;
+    } else {
+        console.log(JSON.stringify(payment));
+        res.render('success', { payerId, paymentId, payment});
+    }
+  });
+});
+
+app.get('/cancel', (req, res) => res.send('Cancelled'));
+
+app.listen(3000, () => console.log('Server Started'));
