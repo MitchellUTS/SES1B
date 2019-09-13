8fc2f5ef957c2b34fcabdcde5e873581eac900bf
MitchellUTS <13248202@student.uts.edu.au>
2019-09-13 14:19:12 +1000

    Paypal feature intergrated with the core application.

--- api/app.js
+++ api/app.js
@@ -1,87 +1,129 @@
-const express = require('express');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var testAPIRouter = require('./routes/testAPI');
 const ejs = require('ejs');
 const paypal = require('paypal-rest-sdk');
 
+const port = 3000;
+
+var app = express();
+
 paypal.configure({
-'mode': 'sandbox', //sandbox or live
-'client_id': 'AYrqSddTaoMoT0YNT_g45A1D03qeu37-NEUS2CyIx6ZTqxi4nFnsScO7sagv63vr1xVLpjEL-EiV0_OX',
-'client_secret': 'EBLPCMU4LF5SzH5rUEZkYT3tZBSyL5b3eUFwvjClSJTA8HzKKJTSKguRBlKkNphMbDjIueMeLbepNeSa'
+  'mode': 'sandbox', //sandbox or live
+  'client_id': 'AYrqSddTaoMoT0YNT_g45A1D03qeu37-NEUS2CyIx6ZTqxi4nFnsScO7sagv63vr1xVLpjEL-EiV0_OX',
+  'client_secret': 'EBLPCMU4LF5SzH5rUEZkYT3tZBSyL5b3eUFwvjClSJTA8HzKKJTSKguRBlKkNphMbDjIueMeLbepNeSa'
 });
 
-const app = express();
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade'); removed by Mitch
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index'));
 
+app.use(logger('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/testAPI', testAPIRouter);
+
 app.post('/pay', (req, res) => {
-const create_payment_json = {
-"intent": "sale",
-"payer": {
-    "payment_method": "paypal"
-},
-"redirect_urls": {
-    "return_url": "http://localhost:3000/success",
-    "cancel_url": "http://localhost:3000/cancel"
-},
-"transactions": [{
-    "item_list": {
+  const create_payment_json = {
+    "intent": "sale",
+    "payer": {
+      "payment_method": "paypal"
+    },
+    "redirect_urls": {
+      "return_url": "http://localhost:3000/success",
+      "cancel_url": "http://localhost:3000/cancel"
+    },
+    "transactions": [{
+      "item_list": {
         "items": [{
-            "name": "Intel Core i5",
-            "sku": "001",
-            "price": "299.00",
-            "currency": "AUD",
-            "quantity": 1
+          "name": "Intel Core i5",
+          "sku": "001",
+          "price": "299.00",
+          "currency": "AUD",
+          "quantity": 1
         }]
-    },
-    "amount": {
+      },
+      "amount": {
         "currency": "AUD",
         "total": "299.00"
-    },
-    "description": "A CPU"
-}]
-};
-
-paypal.payment.create(create_payment_json, function (error, payment) {
-if (error) {
-  throw error;
-} else {
-  for(let i = 0;i < payment.links.length;i++){
-    if(payment.links[i].rel === 'approval_url'){
-      res.redirect(payment.links[i].href);
+      },
+        "description": "A CPU"
+    }]
+  };
+  
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+      throw error;
+    } else {
+      for(let i = 0;i < payment.links.length;i++){
+        if(payment.links[i].rel === 'approval_url'){
+          res.redirect(payment.links[i].href);
+        }
+      }
     }
-  }
-}
-});
+  });
 
 });
-
-app.get('/success', (req, res) => {
-const payerId = req.query.PayerID;
-const paymentId = req.query.paymentId;
-const payment = req.query.payment;
-
-const execute_payment_json = {
-"payer_id": payerId,
-"transactions": [{
-    "amount": {
-        "currency": "AUD",
-        "total": "299.00"
+  
+  app.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+    const payment = req.query.payment;
+    
+    const execute_payment_json = {
+    "payer_id": payerId,
+    "transactions": [{
+        "amount": {
+            "currency": "AUD",
+            "total": "299.00"
+        }
+    }]
+  };
+  
+  paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+    if (error) {
+        console.log(error.response);
+        throw error;
+    } else {
+        console.log(JSON.stringify(payment));
+        res.render('success', { payerId, paymentId, payment});
     }
-}]
-};
-
-paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
-if (error) {
-    console.log(error.response);
-    throw error;
-} else {
-    console.log(JSON.stringify(payment));
-    res.render('success', { payerId, paymentId, payment});
-}
+  });
 });
-});
-
+  
 app.get('/cancel', (req, res) => res.send('Cancelled'));
+  
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.listen(3000, () => console.log('Server Started'));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+app.listen(port, () => console.log("Server Started. \nListening on Port:" + port + "\nPress Ctrl + C to stop the server.\n"));
