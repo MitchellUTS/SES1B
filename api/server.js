3f6b637c2204c826adfc9408e5d192538f0471dd
ryandawson <dawson.ryan24@gmail.com>
2019-09-20 13:45:42 +1000

    Added email

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
   }
   
+  'use strict';
+  const nodemailer = require('nodemailer');
   var createError = require('http-errors');
   var express = require('express');
   var path = require('path');
@@ -29,16 +31,6 @@ if (process.env.NODE_ENV !== 'production') {
   
   const port = 3000;
   const users = []
-
-  /*const mysql = require('mysql'); MUST USE 'npm install mysql' for this to work
-  const pool = mysql.createPool({
-	  connectionLimit : 100,
-	  host : 'localhost',
-	  database : 'test',
-	  user : 'root',
-	  password : 'pass123',
-	  debug : false
-  });*/
   
   app.set('views', path.join(__dirname, 'views'));
   app.set('view-engine', 'ejs')
@@ -81,6 +73,7 @@ if (process.env.NODE_ENV !== 'production') {
         email: req.body.email,
         password: hashedPassword
       })
+      sendEmail(req.body.email, 'Module Email', 'yo').catch(console.error);
       res.redirect('/login')
     } catch {
       res.redirect('/register')
@@ -106,106 +99,6 @@ if (process.env.NODE_ENV !== 'production') {
     }
     next()
   }
-
-  //query rows in the table
-
-  /*function queryRow(data) {
-    let selectQuery = 'SELECT * FROM ? WHERE ? = ?';    
-    let query = mysql.format(selectQuery,[data.table,data.field1Name, data.field1Value]);
-    // query = SELECT * FROM `user` where `EmailAddress` = '12875833@student.uts.edu.au'
-    pool.query(query,(err, data) => {
-        if(err) {
-            console.error(err);
-            return;
-        }
-        // rows fetch
-        return data;
-    });
-}*/
-
-//add rows in the table
-
-/*function addRow(data) {
-  let insertQuery = 'INSERT INTO ? (?,?,?,?,?,?,?) VALUES (?,?,?,?,?,?,?)';
-  let query = mysql.format(insertQuery,[data.table,data.field1Name,data.field2Name,data.field3Name,data.field4Name,data.field5Name,data.field6Name,data.field7Name,data.field1Value,data.field2Value,data.field3Value,data.field4Value,data.field5Value,data.field6Value,data.field7Value]);
-  pool.query(query,(err, response) => {
-      if(err) {
-          console.error(err);
-          return;
-      }
-      // rows added
-      console.log(response.insertId);
-  });
-}*/
-  
-//update rows
-
-/*function updateRow(data) {
-  let updateQuery = "UPDATE ? SET ? = ? WHERE ? = ?";
-  let query = mysql.format(updateQuery,[data.table,data.field1Name,data.field1Value,data.field2Name,data.field2Value]);
-  // query = UPDATE `user` SET `EmailAddress`='12875822@student.uts.edu.au' WHERE `Name`='Matt'
-  pool.query(query,(err, response) => {
-      if(err) {
-          console.error(err);
-          return;
-      }
-      // rows updated
-      console.log(response.affectedRows);
-  });
-}*/
-
-// timeout just to avoid firing query before connection happens
-
-//Insert query call
-/*setTimeout(() => {
-  // call the function
-  addRow({
-      "table": user,
-      
-      "field1Name": "ID",
-      "field2Name": "Name",
-      "field3Name": "DateOfBirth",
-      "field4Name": "EmailAddress",
-      "field5Name": "PhoneNumber",
-      "field6Name": "Verified",
-      "field7Name": "Password"
-
-      "field1Value": "2",
-      "field2Value": "Matt",
-      "field3Value": "2000-01-01 00:00:00",
-      "field4Value": "12875833@student.uts.edu.au",
-      "field5Value": "0412345678",
-      "field6Value": "000",
-      "field7Value": "HashedPassword"
-  });
-},5000);*/
-
-//Select query call
-/*setTimeout(() => {
-  // call the function
-  // select rows
-  queryRow({
-    "table": "user",
-
-    "field1Name": "EmailAddress",
-    "field1Value": "12875833@student.uts.edu.au"
-  });
-},5000);*/
-
-//Update query call
-/*setTimeout(() => {
-  // call the function
-  // update row
-  updateRow({
-      "table": "Matt",
-
-      "field1Name": "EmailAddress",
-      "field2Name": "Name",
-
-      "field1Value": "12875822@student.uts.edu.au",
-      "field2Value": "Matt"
-  });
-},5000);*/
 
 paypal.configure({
   'mode': 'sandbox', //sandbox or live
@@ -298,6 +191,40 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendEmail(recipients, subject, body) {
+    // Generate test SMTP service account from ethereal.email
+    // Only needed if you don't have a real mail account for testing
+    //let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'softwarestudio1b@gmail.com', // generated ethereal user
+            pass: 'Sasuke12345!' // generated ethereal password
+        }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"No reply" <ayushdallakoti18@gmail.com>', // sender address
+        to: recipients, // list of receivers
+        subject: subject, // Subject line
+        text: body // plain text body
+        //html: '<b>Your email has been verified</b>' // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    //console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
 
 module.exports = app;
 
